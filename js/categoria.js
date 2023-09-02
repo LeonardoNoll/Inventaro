1c0077259f8c573f0a8703d0af4b47edac4ad322
Leonardo <leonoll8@hotmail.com>
2023-09-01 21:14:57 -0300

    //TODO terminar função addItem

--- js/categoria.js
+++ js/categoria.js
@@ -30,4 +30,33 @@ function manualChange(){
         console.log('num')
         console.log(qnt)
     }
-}  
+}
+
+function addNewItem(){
+    // let item = document.querySelector('.itensWrapper').cloneNode(true)
+    let itemName = window.prompt('Qual o nome deste item: ')
+    let item = document.createElement('span')
+    item.classList.add('itensWrapper')
+    
+        let left = document.createElement('div')
+        left.classList.add('itensWrapperLeft')
+
+            let X = document.createElement('img')
+            X.src = 'imgs/plus-positive-add-mathematical-symbol.png'
+            X.classList.add('itemDelete')
+
+            let title = document.createElement('h1')
+            title.classList.add('itemName')
+            title.innerText = itemName
+
+        let right = document.createElement('div')
+        right.classList.add('itensWrapperRight')
+
+            let minus = document.createElement('img')
+            minus.src = 'imgs/minus-sign.png'
+            minus.classList.add('itensButtons')
+            minus.addEventListener(onclick, changeQnt(0,1))
+
+}
+
+//TODO terminar a função addNewItem
